49d076c00faee1ac63764a79ba5bbe06c5e22b4b
Arin T <ukkabanana@Mac-and-Cheese.local>
2020-11-03 00:11:22 +0700

    Adding numbers parameters to Game page

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory,
 } from "react-router-dom";
-import { Box, Grid } from '@chakra-ui/core'
+import { Box, Grid, Button, ThemeProvider, CSSReset } from '@chakra-ui/core'
 import NameForm from './components/NameForm'
 import Waiting from './pages/Waiting/index'
 import Game from './pages/Game/index'
@@ -14,37 +15,51 @@ import Round from './pages/Round/index'
 import Result from './pages/Result/index'
 
 function App() {
-
+  let history = useHistory();
   const submit = (name) => {
     // handle socket submit
     console.log(`submit ${name}`)
   }
+  // const goToGame = () => {
+  //   history.push("/game")
+  // };
 
   return (
-    <Router>
 
-      <Switch>
-        <Route exact path="/">
-          <Grid className="container">
-            <Box className="box">
-              <NameForm submit={submit} />
-            </Box>
-          </Grid>
-        </Route>
-        <Route path="/game">
-          <Game />
-        </Route>
-        <Route path="/waiting">
-          <Waiting />
-        </Route>
-        <Route path="/round">
-          <Round />
-        </Route>
-        <Route path="/result">
-          <Result />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider>
+      <CSSReset />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Grid className="container">
+              <Box className="box">
+                <NameForm submit={submit} />
+                <Link to="/game">
+                  <Button
+                    my="4"
+                  // onClick={goToGame}
+                  >
+                    Go to Game
+                  </Button>
+                </Link>
+              </Box>
+            </Grid>
+          </Route>
+          <Route path="/game">
+            <Game />
+          </Route>
+          <Route path="/waiting">
+            <Waiting />
+          </Route>
+          <Route path="/round">
+            <Round />
+          </Route>
+          <Route path="/result">
+            <Result />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
 
   )
 }
