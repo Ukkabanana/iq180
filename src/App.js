8f0426a3bbdbac274a4e10ac028f102fe3d0e017
Arin T <ukkabanana@Mac-and-Cheese.local>
2020-11-04 14:50:42 +0700

    initiate socket connection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,13 +13,24 @@ import Waiting from './pages/Waiting/index'
 import Game from './pages/Game/index'
 import Round from './pages/Round/index'
 import Result from './pages/Result/index'
-
+import io from 'socket.io-client'
 function App() {
+  const socket = io("https://netcentric-iq180.herokuapp.com");
   let history = useHistory();
+  const [joinRoomResult, setJoinRoomResult] = useState({});
   const submit = (name) => {
     // handle socket submit
-    console.log(`submit ${name}`)
+    console.log("submit" + name)
+    socket.emit("JOIN_ROOM", { name }) //define by backend.
   }
+  useEffect(() => {
+    socket.on("JOIN_ROOM_RESULT", (result) => {
+      console.log(result)
+      setJoinRoomResult(result);
+    })//wait for result of joining game from backend.
+  }, [])
+
+
   // const goToGame = () => {
   //   history.push("/game")
   // };
@@ -49,7 +60,7 @@ function App() {
             <Game />
           </Route>
           <Route path="/waiting">
-            <Waiting />
+            <Waiting joinRoomResult={joinRoomResult} />
           </Route>
           <Route path="/round">
             <Round />
